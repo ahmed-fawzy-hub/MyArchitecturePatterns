2a409b6caa995c12c2a28ba7a35d1541c77b42ef
ahmed-fawzy-hub <af348565@gmail.com>
2023-12-29 20:01:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myarchitecturepatterns/model/Wisdom.kt
@@ -0,0 +1,6 @@
+package com.example.myarchitecturepatterns.model
+
+data class Wisdom(
+    val content: String,
+    val publishDate: String
+)
---
+++ app/src/main/java/com/example/myarchitecturepatterns/ui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myarchitecturepatterns.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.myarchitecturepatterns.R
+import com.example.myarchitecturepatterns.databinding.ActivityMainBinding
+import com.example.myarchitecturepatterns.viewModels.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel=viewModel
+        binding.lifecycleOwner=this
+        setup()
+
+    }
+
+    private fun setup() {
+
+        viewModel.getUserInfo()
+
+        binding.buttonFetchWisdom.setOnClickListener {
+            viewModel.getWisdom()
+        }
+        viewModel.currentUser.observe(this,{
+            binding.textUserName.text=it.username
+        })
+        viewModel.wisdom.observe(this,{
+            binding.apply {
+                textDate.text=it.publishDate
+                textContent.text=it.content
+            }
+        })
+    }
+
+
+}
